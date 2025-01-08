2b16f936a4c9988137eb5e615e69161f2abaa0c6
2411013daejinsenhskr <24_11013@daejin.sen.hs.kr>
2025-01-08 11:22:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import stremlit as st
-
+import random
 st.title("나의 첫번째 앱")
 import streamlit as st
 
 st.button("Reset", type="primary")
-if st.button("Say hello"):
-    st.write("Why hello there")
+if st.button("난수 생성"):
+    st.write(random.randint(1,1000))
 else:
-    st.write("Goodbye")
+    st.write("안녕")
 
 if st.button("Aloha", type="tertiary"):
     st.write("Ciao")
