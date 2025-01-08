45c6edebf37da26dd16c3c3a42fd115be1875a70
2411013daejinsenhskr <24_11013@daejin.sen.hs.kr>
2025-01-08 11:38:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,7 @@ import streamlit as st
 st.title("이미지 링크로 이미지 띄우기")
 
 # 사용자로부터 이미지 URL 입력 받기
-image_url = st.text_input("이미지 링크를 입력하세요:")
-
-# 테스트용 이미지 URL
-image_url = "https://healingfactory.co.kr/data/files/dee27b6e7d998faa5dbeb3e36762c28d.jpg"
-
-# 이미지 출력
-st.image(image_url, caption="테스트용 이미지", use_column_width=True)
-
-# URL 출력 및 복사 기능
-st.markdown(f"[이미지 링크 복사하기]({image_url})", unsafe_allow_html=True)
+image_url = st.text_input("이미지 링크를 입력하세요 (예시:https://healingfactory.co.kr/data/files/dee27b6e7d998faa5dbeb3e36762c28d.jpg)")
 
 # "확인" 버튼 클릭 시 이미지 출력
 if st.button("확인"):
